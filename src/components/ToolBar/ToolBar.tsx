9ec5d655397c30dbb69f63f3df57dba7c296ec6d
Enzo Gonzalez <info@enzogonzalez.es>
2023-08-28 19:26:50 +0200

    copy code to clipboard feature added

--- src/components/ToolBar/ToolBar.tsx
+++ src/components/ToolBar/ToolBar.tsx
@@ -1,15 +1,32 @@
 import ButtonTool from "./ButtonTool";
 import visibleIcon from "/icons/view.svg";
 import hiddeIcon from "/icons/hide-3.svg";
+import codeIcon from "/icons/code.svg";
 import { useState } from "react";
 import { toggleOpacityPins } from "./PaletteActions/togglePinsInCanvas";
+import { RootState } from "../../types/types";
+import { pinsToCss } from "../../utils/colorUtils";
+import { useSelector } from "react-redux";
 
 export default function Toolbar() {
   const [isPinVisible, setIsPinVisible] = useState(true);
+  const { pins } = useSelector((state: RootState) => state.canvas);
 
   const handleToggleVisible = () => {
     setIsPinVisible(isPinVisible ? false : true);
     toggleOpacityPins(isPinVisible);
+  };
+
+  const handleCopyCodeToClipboard = () => {
+    const code = pinsToCss(pins);
+    navigator.clipboard
+      .writeText(code)
+      .then(() => {
+        console.log("Código copiado al portapapeles exitosamente");
+      })
+      .catch((error) => {
+        console.error("No se pudo copiar el código: ", error);
+      });
   };
 
   return (
@@ -19,6 +36,10 @@ export default function Toolbar() {
         icon={
           <img src={isPinVisible ? visibleIcon : hiddeIcon} alt="Toggle Pins" />
         }
+      />
+      <ButtonTool
+        onClick={handleCopyCodeToClipboard}
+        icon={<img src={codeIcon} alt="Copy code to clipboard" />}
       />
     </div>
   );
