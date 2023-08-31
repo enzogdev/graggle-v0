16b1c46cfd12545f282785be8d46aee3a3dba3e2
Enzo Gonzalez <info@enzogonzalez.es>
2023-08-31 08:40:06 +0200

    update footer data

--- src/components/Navigation/Footer.tsx
+++ src/components/Navigation/Footer.tsx
@@ -1,9 +1,17 @@
 export default function Footer() {
   return (
     <footer className="w-full flex justify-between gap-5 items-center text-xs dark:text-gray-100">
-      <span>This is pre-before alpha version</span>
-      <b>Be patient in the process, you are doing this because you love it</b>
-      <span>Created by Lorenzo González</span>
+      <span>This is alpha version</span>
+      <span>
+        Created with ❤️ by{" "}
+        <a
+          className="underline underline-offset-4"
+          href="https://www.linkedin.com/in/lorenzo-gonz%C3%A1lez-jim%C3%A9nez/"
+          target="_blank"
+        >
+          Lorenzo González
+        </a>
+      </span>
     </footer>
   );
 }
